326fc7b088fb5ff28e3b35c1599fcd402e096baf
Luis Alvarez <ingenierodesarrollo6@cemcolombia.co>
2021-07-29 20:18:48 -0500

    Inicial componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './shared/components/menu/menu.component';
+import { UrlComponent } from './shared/components/url/url.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    UrlComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
-  ],
+  ], 
   providers: [],
   bootstrap: [AppComponent]
 })
